7289d29b8176eafd456e6347bbceb1bed8946594
SnehaMarathe <doit13580@gmail.com>
2025-10-08 11:38:33 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/tco2display/MainActivity.kt
+++ app/src/main/java/com/example/tco2display/MainActivity.kt
@@ -286,13 +286,13 @@ private fun WhatThisMeansRowSingle(modifier: Modifier, tco2: Double?, color: Col
             append("As Valuable To Our ")
         }
         withStyle(SpanStyle(fontSize = 50.sp)) { append(" 🌎") }
-        withStyle(SpanStyle(color = color.copy(alpha = 0.9f), (fontSize = 22.sp)) { append(" As ") }
+        withStyle(SpanStyle(color = color.copy(alpha = 0.9f), fontSize = 22.sp) { append(" As ") }
         withStyle(SpanStyle(color = color, fontSize = 32.sp, fontWeight = FontWeight.ExtraBold)) {
             append(nf.format(midTrees))
         }
         withStyle(SpanStyle(fontSize = 50.sp)) { append(" 🌳") }
 
-        withStyle(SpanStyle(color = color.copy(alpha = 0.9f), (fontSize = 22.sp)) { append(" Working for a Greener Future ") }
+        withStyle(SpanStyle(color = color.copy(alpha = 0.9f), fontSize = 22.sp) { append(" Working for a Greener Future ") }
 
         withStyle(SpanStyle(fontSize = 22.sp)) { append("🌿") }
     }
